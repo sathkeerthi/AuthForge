209777ee7eb08aba45d69d2811638341b0c24220
Sathkeerthi Y Agnihothri <syagni1947@gmail.com>
2025-11-11 18:55:44 +0530

    Initial commit

---
+++ authforge-backend/src/main/java/com/sathkeerthi/authforge/authforge_backend/AuthforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sathkeerthi.authforge.authforge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthforgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthforgeBackendApplication.class, args);
+	}
+
+}
---
+++ authforge-backend/src/test/java/com/sathkeerthi/authforge/authforge_backend/AuthforgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathkeerthi.authforge.authforge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthforgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
